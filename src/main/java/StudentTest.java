218576db67663bcad85af6603172d2d1c4442788
Joshua <joshwilk40@gmail.com>
2023-12-05 15:36:32 +0000

    update

--- src/main/java/StudentTest.java
+++ src/main/java/StudentTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class StudentTest {
     @Test
-    public void belowZero() {
+    public void testBelowZero() {
         Student std = new Student();
         assertThrows(IllegalArgumentException.class, () -> {
             std.getAttendanceGrade(-1);
@@ -17,7 +17,7 @@ public class StudentTest {
     }
 
     @Test
-    public void muchBelowZero(){
+    public void testMuchBelowZero(){
         Student std = new Student();
         assertThrows(IllegalArgumentException.class, () -> {
             std.getAttendanceGrade(-42);
@@ -25,7 +25,7 @@ public class StudentTest {
     }
 
     @Test
-    public void above100() {
+    public void testAbove100() {
         Student std = new Student();
         assertThrows(IllegalArgumentException.class, () -> {
             std.getAttendanceGrade(101);
@@ -33,7 +33,7 @@ public class StudentTest {
     }
 
     @Test
-    public void muchAbove100() {
+    public void testMuchAbove100() {
         Student std = new Student();
         assertThrows(IllegalArgumentException.class, () -> {
             std.getAttendanceGrade(142);
@@ -41,55 +41,55 @@ public class StudentTest {
     }
 
     @Test
-    public void absent() {
+    public void testAbsent() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.ABSENT, std.getAttendanceGrade(0));
     }
 
     @Test
-    public void lowVeryPoor() {
+    public void testLowVeryPoor() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.VERY_POOR, std.getAttendanceGrade(1));
     }
 
     @Test
-    public void highVeryPoor() {
+    public void testHighVeryPoor() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.VERY_POOR, std.getAttendanceGrade(29));
     }
 
     @Test
-    public void lowAverage() {
+    public void testLowAverage() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.AVERAGE, std.getAttendanceGrade(30));
     }
 
     @Test
-    public void highAverage() {
+    public void testHighAverage() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.AVERAGE, std.getAttendanceGrade(69));
     }
 
     @Test
-    public void lowGood() {
+    public void testLowGood() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.GOOD, std.getAttendanceGrade(70));
     }
 
     @Test
-    public void highGood() {
+    public void testHighGood() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.GOOD, std.getAttendanceGrade(89));
     }
 
     @Test
-    public void lowVeryGood() {
+    public void testLowVeryGood() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.VERY_GOOD, std.getAttendanceGrade(90));
     }
 
     @Test
-    public void highVeryGood() {
+    public void testHighVeryGood() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.VERY_GOOD, std.getAttendanceGrade(100));
     }
